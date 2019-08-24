88abb5cae71efce915e0a6aa38ab69c6e2236b21
Lakshmi Narasimhan <lakshminp@pop-os.localdomain>
2019-08-24 06:04:21 +0530

    OpenShift related changes.

--- server.js
+++ server.js
@@ -4,6 +4,30 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
       mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL;
+var mongoURLLabel = "";
+
+if (mongoURL == null) {
+  var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+  // If using plane old env vars via service discovery
+  if (process.env.DATABASE_SERVICE_NAME) {
+    mongoHost = process.env.DATABASE_SERVICE_NAME;
+    mongoPort = "27017";
+    mongoDatabase = process.env.MONGODB_DATABASE;
+    mongoPassword = process.env.MONGODB_PASSWORD;
+    mongoUser = process.env.MONGODB_USER;
+
+  }
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+  }
+}
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
